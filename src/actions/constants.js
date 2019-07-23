67e0ed01967b73ae5abcd9238870a6bf7df29eee
TATY <tatyccsilva@gmail.com>
2019-07-23 15:23:40 +0200

    Set ngrok url

--- src/actions/constants.js
+++ src/actions/constants.js
@@ -1,3 +1,4 @@
-export const baseUrl = 'https://glacial-thicket-13029.herokuapp.com'
+// export const baseUrl = 'https://glacial-thicket-13029.herokuapp.com' //heroku
+// export const baseUrl = 'http://70ead992.ngrok.io' //ngrok
 
-// export const baseUrl = 'http://localhost:4000'
+export const baseUrl = 'http://localhost:4000'
