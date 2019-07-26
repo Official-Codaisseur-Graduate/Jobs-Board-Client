e480369dbfa6746ead71b2f0a306262b6557838d
TATY <tatyccsilva@gmail.com>
2019-07-26 13:57:40 +0200

    Refactor jobs actions

--- src/actions/jobs.js
+++ src/actions/jobs.js
@@ -8,14 +8,8 @@ const jobsFetched = jobs => ({
   jobs
 })
 
-// export const initializeJobs = () => (dispatch, getState) => {
-//   if (getState().jobs) return
-//   dispatch( searchJobs({query: 'Junior Developer', city: 'Amsterdam'}) )
-// }
-
 export const initializeJobs = () => (dispatch, getState) => {
   if (getState().jobs) return
-  // console.log('getstate', getState().jobs === true)
   dispatch( searchJobs({query: 'Junior Developer', city: 'Amsterdam'}) )
 }
 
@@ -32,38 +26,3 @@ export const searchJobs = (query) => (dispatch) => {
       console.error(error);
     });
 }
-
-// export const searchJobs = (query) => (dispatch) => {
-//   dispatch(jobsFetched(null))
-
-//   request
-//     .get(`${baseUrl}/jobs`)
-//     .query(query)
-//     .then(response => {
-//       dispatch(jobsFetched(response.body));
-//     })
-//     .catch(error => {
-//       console.error(error);
-//     });
-// }
-
-
-
-
-// export const JOBS_COMPANY_FETCHED = "JOBS_COMPANY_FETCHED";
-
-// const jobsCompanyFetched = company => ({
-//   type: JOBS_COMPANY_FETCHED,
-//   company
-// })
-
-// export const findMatchingCompany = (companyName) => (dispatch) => {
-//   request
-//     .get(`${baseUrl}/companies/jobs/${companyName}`)
-//     .then(response => {
-//       dispatch(jobsCompanyFetched(response.body))
-//     })
-//     .catch(error => {
-//       console.error(error)
-//     })
-// }
