f2ca7fd3974ef83b6190f23f8953b78e18a0f422
TATY <tatyccsilva@gmail.com>
2019-07-29 13:05:48 +0200

    Showing job details

--- src/App.js
+++ src/App.js
@@ -19,8 +19,8 @@ class App extends Component {
           <Route exact path="/" component={HomePage} />
           <Route exact path="/jobs" component={JobsListContainer} />
           <Route exact path="/companies" component={CompaniesListContainer} />
-          <Route exact path="/jobs/:name" component={JobsDetailsContainer} />
           <Route exact path="/company/:id" component={CompanyDetailsContainer} />
+          <Route exact path="/jobs/:id" component={JobsDetailsContainer} />
           <Route path="/graduates" component={GraduatesStatsListContainer} />
         </div>
       </Provider>
