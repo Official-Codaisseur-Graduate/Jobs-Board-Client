46cd9f41fecfb060c7428425a23939becdfd9b7c
Meenakshi Venkat <meenak.venkat@gmail.com>
2019-09-02 11:27:23 +0200

    Modify to heroku URL

--- src/actions/constants.js
+++ src/actions/constants.js
@@ -1,6 +1,6 @@
 
-// export const baseUrl = 'https://sleepy-tor-95168.herokuapp.com'
-export const baseUrl = 'http://localhost:4000'
+export const baseUrl = 'https://frozen-meadow-51398.herokuapp.com/'
+//export const baseUrl = 'http://localhost:4000'
 
 
 
